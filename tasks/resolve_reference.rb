e290a2d2092ee2997049620a3362359aa7f26dd7
Corey Osman <corey@nwops.io>
2020-08-12 10:25:41 -0700

    Adds group by compose groups

--- tasks/resolve_reference.rb
+++ tasks/resolve_reference.rb
@@ -14,11 +14,30 @@ require_relative task_helper
 # Retrieves hosts from the docker host
 class DockerInventory < TaskHelper
   TRANSPORT = 'docker://'
+  attr_reader :options
 
   def resolve_reference(opts)
-    format = opts[:format] || 'targets'
+    @options = opts
+    format = opts[:format] || 'groups'
     group_name_prefix = opts[:group_name_prefix] || ''
     parse_data(containers, format, group_name_prefix)
+  end
+
+  def container_data(id)
+    data = `docker inspect #{id}`
+    JSON.parse(data).first
+  end
+
+  def compose_dir(data)
+    data['Config']['Labels']["com.docker.compose.project.working_dir"]
+  end
+
+  def container_hostname(data)
+    data['Config']['Hostname']
+  end
+
+  def compose_project(data)
+    data['Config']['Labels']["com.docker.compose.project"]
   end
 
   def containers
@@ -26,19 +45,25 @@ class DockerInventory < TaskHelper
   end
 
   def parse_data(dataset, format, group_name_prefix)
-    # if format == 'groups'
-    #   dataset.each_with_object({}) do |container_id, groups|
-    #     unless groups.key?(group_name)
-    #       groups[group_name] = {
-    #         name: group_name,
-    #         targets: [],
-    #       }
-    #     end
-    #     groups[group_name][:targets] << { uri: container_id }
-    #   end
-    #   groups.values
-    if format == 'targets'
-      dataset.map { |container_id| {uri: "#{TRANSPORT}#{container_id}" } }
+    if format == 'groups'
+      groups = dataset.each_with_object({}) do |container_id, groups|
+        cdata = container_data(container_id)
+        group_name = compose_project(cdata) || 'ungrouped_containers'
+        unless groups.key?(group_name)
+          groups[group_name] = {
+            name: group_name,
+            targets: [],
+          }
+        end
+        name = options[:use_hostname] ? container_hostname(container_data(container_id)) : container_id
+        groups[group_name][:targets] << {uri: "#{TRANSPORT}#{name}" } 
+      end
+      groups.values
+    elsif format == 'targets'
+      dataset.map do |container_id| 
+        name = options[:use_hostname] ? container_hostname(container_data(container_id)) : container_id
+        {uri: "#{TRANSPORT}#{name}" } 
+      end
     else
       raise TaskHelper::Error.new("Unknown format: #{format}", 'bad/data')
     end
