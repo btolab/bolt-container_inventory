08526db1c404694aeffaea9bb7443dc52ed43f73
Corey Osman <corey@nwops.io>
2020-08-25 09:29:44 -0700

    Adds additional docs

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 # Bolt Container Inventory
 
-This is a bolt container inventory plugin used to enumerate the currently running containers on your local system. Currently there is limited functionality but the plan is to add additional features to cover more runtimes and use cases.
+This is a bolt container inventory plugin used to enumerate the currently running containers on your local docker host. Currently there is limited functionality but the plan is to add additional features to cover more runtimes and use cases.
 
 ## Description
 Bolt can use docker as a transport but there hasn't been a way to easily list the containers without this plugin. This plugin will run `docker ps --filter status=running -q` from the command line to return the current list of running containers.  At the moment there is no way to alter this list with configuration.
-
-
 
 ## Setup
 You will need to have the `puppetlabs/ruby_task_helper` module and this module installed in your bolt module path. 
@@ -16,15 +14,18 @@ Ensure you inventory uses the plugin `container_inventory` name like below.
 ```
 ---
 groups:
-  - name: my_containers
+  - _plugin: container_inventory
     config:
       transport: docker
-    targets:
-      _plugin: container_inventory
+    parameters: 
+      format: groups
+      use_hostname: true
 ```
 
 
 ## Usage
+
+If you are using docker compose you can format the inventory as groups and return only containers from a specific docker-compose file. In this example standard is the name of my project in docker-compose. Because of this we can filter out only containers in the standard project and group them.  By default if multiple docker-compose projects are running all containers will be returned.  With bolt you can filter using the `--targets=standard` or whatever your project name is. 
 
 ```
 bolt command run 'uname -i' --targets=standard -i inventory.yaml 
--- tasks/resolve_reference.rb
+++ tasks/resolve_reference.rb
@@ -14,6 +14,21 @@ require_relative task_helper
 class DockerInventory < TaskHelper
   attr_reader :options
 
+  def task(opts)
+    opts[:format] ||= 'groups'
+    opts[:use_hostname] = true if opts[:use_hostname].nil? 
+    opts[:group_name_prefix] ||= ''
+    opts[:ungrouped_name] || 'ungrouped_containers'
+    data = resolve_reference(opts)
+    return { value: data }
+  rescue TaskHelper::Error => e
+    puts opts.inspect
+    # ruby_task_helper doesn't print errors under the _error key, so we have to
+    # handle that ourselves
+    return { _error: e.to_h }
+  end
+  
+  # @return [Hash]
   def resolve_reference(opts)
     @options = opts
     format = opts[:format] 
@@ -21,32 +36,40 @@ class DockerInventory < TaskHelper
     parse_data(containers, format, group_name_prefix)
   end
 
+  private  
+
+  # @return [Array] A array of container data
   def container_data(id)
     data = `docker inspect #{id}`
     JSON.parse(data).first
   end
 
+  # @return [String] - the directory of the compose project 
   def compose_dir(data)
-    data['Config']['Labels']["com.docker.compose.project.working_dir"]
+    data['Config']['Labels'].fetch("com.docker.compose.project.working_dir", nil)
   end
 
+  # @return [String] - the hostname of the container
   def container_hostname(data)
     data['Config']['Hostname']
   end
 
+  # @return [String] - the name of the compose project or nil 
   def compose_project(data)
-    data['Config']['Labels']["com.docker.compose.project"]
+    data['Config']['Labels'].fetch("com.docker.compose.project", nil)
   end
 
+  # @return [Array] - array of container ids
   def containers
-    `docker ps -q --filter status=running`.split("\n")
+    out = `docker ps -q --filter status=running 2>&1`
+    $?.success? ? out.split("\n") : []
   end
 
   def parse_data(dataset, format, group_name_prefix)
     if format == 'groups'
       groups = dataset.each_with_object({}) do |container_id, groups|
         cdata = container_data(container_id)
-        group_name = compose_project(cdata) || @options['ungrouped_name']
+        group_name = compose_project(cdata) || options[:ungrouped_name]
         unless groups.key?(group_name)
           groups[group_name] = {
             name: group_name,
@@ -65,20 +88,6 @@ class DockerInventory < TaskHelper
     else
       raise TaskHelper::Error.new("Unknown format: #{format}", 'bad/data')
     end
-  end
-
-  def task(opts)
-    opts[:format] ||= 'groups'
-    opts[:use_hostname] = true if opts[:use_hostname].nil? 
-    opts[:group_name_prefix] ||= ''
-    opts[:ungrouped_name] || 'ungrouped_containers'
-    data = resolve_reference(opts)
-    return { value: data }
-  rescue TaskHelper::Error => e
-    puts opts.inspect
-    # ruby_task_helper doesn't print errors under the _error key, so we have to
-    # handle that ourselves
-    return { _error: e.to_h }
   end
 end
 
